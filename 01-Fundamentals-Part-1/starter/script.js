923eb646bd060530eae31024751a43a82b51c606
Jakub Piasta <kuba.piasta@hotmail.com>
2023-03-24 14:41:05 +0100

    Done Conditional Operator and assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -251,6 +251,7 @@ if(hasDriverLicense && hasGoodVision && !isTired) {
 }
 */
 
+/*
 const day = 'thursday';
 
 switch(day) {
@@ -275,3 +276,24 @@ switch(day) {
     default:
         console.log(`Not a valid day!`);
 }
+*/
+
+/*
+const age = 23;
+age >= 18 ? console.log(`I like to drink wine`) : console.log(`I like to drink water`);
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+*/
+
+
