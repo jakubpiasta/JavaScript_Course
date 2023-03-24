52b4e2ccf898670b0703a66ef86a763814a82774
Jakub Piasta <kuba.piasta@hotmail.com>
2023-03-24 14:17:18 +0100

    Done The switch Statement Lecture and assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -142,3 +142,29 @@ if (avgScoreDolphins > avgScoreKoalas && avgScoreDolphins >= 100) {
     console.log(`Nobody wins the trophy`);
 }
 */
+
+/*
+// LECTURE: The switch Statement
+const language = 'arabic';
+
+switch(language) {
+    case 'chinese':
+    case 'mandarin':
+        console.log(`MOST number of native speakers`);
+        break;
+    case 'spanish':
+        console.log(`2nd place in number of native speakers`);
+        break;
+    case 'english':
+        console.log(`3rd place`);
+        break; 
+    case 'hindi':
+        console.log(`Number 4`);
+        break;
+    case 'arabic':
+        console.log(`5th most spoken language`);
+        break;
+    default:
+        console.log(`Great language too :D`)
+}
+*/
