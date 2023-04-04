84644c200685b3333033adb1cd49c0cf29604510
Jakub Piasta <kuba.piasta@hotmail.com>
2023-04-04 21:27:48 +0200

    Done Object Methods Lecture and its assignment

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -159,3 +159,23 @@ console.log(`${myCountry.country} has ${myCountry.population} milion ${myCountry
 myCountry.population += 2;
 myCountry["population"] -= 2;
 */
+
+/*
+// LECTURE: Object Methods
+const myCountry = {
+    country: "Poland",
+    capital: "Warsaw",
+    language: "polish",
+    population: 40,
+    neighbours: ["Germany", "Czech Republic", "Slovakia", "Ukraine", "Belarus", "Lithuania", "Russia"],
+    describe: function() {
+        console.log(`${this.country} has ${this.population} milion ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}`)
+    },
+    checkIsland: function() {
+        this.isIsland = this.neighbours.length === 0 ? true : false;
+    }
+};
+
+myCountry.describe();
+myCountry.checkIsland();
+*/
